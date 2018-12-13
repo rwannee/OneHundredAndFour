a7152ed9c34c413aa27eda85a16aa78f90f17965
Randolf Wannee <rwannee@xs4all.nl>
2018-12-13 22:56:00 +0100

    initial files

---
+++ src/main/java/onehundredandfour/Game.java
@@ -0,0 +1,4 @@
+package onehundredandfour;
+
+public class Game {
+}
---
+++ src/test/java/onehundredandfour/GameTest.java
@@ -0,0 +1,8 @@
+package onehundredandfour;
+import java.util.Optional;
+
+import javax.swing.ButtonModel;
+import javax.swing.JRadioButtonMenuItem;
+
+public class GameTest {
+}
